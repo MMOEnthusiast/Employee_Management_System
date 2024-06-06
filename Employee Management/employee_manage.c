ba5fd71bfefd68a22afc9dc394c5635824fdeba2
MMOEnthusiast <143852303+MMOEnthusiast@users.noreply.github.com>
2024-06-06 03:18:15 -0400

    Okay, little bit of progress, I'll stop slacking off and get to this asap tomorrow!

--- Employee Management/employee_manage.c
+++ Employee Management/employee_manage.c
@@ -11,3 +11,28 @@ void display_whole_list(FILE * fp);
 void searchRecord(FILE * fp);
 void printChar(FILE * fp);
 void printHead();
+
+struct emp
+{
+    int id;
+    char name[100];
+    char desgn[10];
+    float sal;
+    char jdate[8];
+    char gender[10];
+    char branch[50];
+    char psaddr[200];
+    char prtaddr[200];
+    char phone[15];
+    char mail[20];
+};
+
+int main()
+{
+    FILE * fp;
+    Employee e;
+    int option;
+    char another;
+
+    if((fp=fopen("employeeInfo.txt", "rb+"))==NULL)
+}
